8c8161fba5a484d8aca74342255f19cdb9e11864
MichalTyczynski12 <105392395+MichalTyczynski12@users.noreply.github.com>
2022-06-05 22:38:38 +0200

    Add files via upload

---
+++ szyfr cezara1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+	public static void Main()
+	{
+		string tekst = "YFELXIKFGXG";
+		int klucz = 20;
+		int n = tekst.Length;
+		string rozwiazanie = "";
+		byte[] bytes = Encoding.ASCII.GetBytes(tekst);
+
+		if (klucz > -26 && klucz < 26)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				if (bytes[i] + klucz > 90) // > Z
+				{
+					if (klucz < 0)
+					{
+						bytes[i] -= Convert.ToByte(Math.Abs((klucz)));
+					}
+					else
+					{
+						bytes[i] += Convert.ToByte((klucz));
+					}
+
+					bytes[i] -= Convert.ToByte(26);
+				}
+				else if (bytes[i] + klucz < 65) // < A
+				{
+					bytes[i] += Convert.ToByte(klucz + 26);
+				}
+				else
+				{
+					if (klucz < 0)
+					{
+						bytes[i] -= Convert.ToByte(Math.Abs((klucz)));
+					}
+                    else
+                    {
+						bytes[i] += Convert.ToByte((klucz));
+					}
+				}
+			}
+			rozwiazanie = Encoding.ASCII.GetString(bytes);
+		}
+
+		Console.WriteLine(rozwiazanie);
+	}
+}
